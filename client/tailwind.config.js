b6e67fba4f0ae842559a7c84dbf7493b374d66e4
tanmayagrwl <tanmayhp78@gmail.com>
2024-03-08 23:33:30 +0530

    chore: create react app with tailwindcss again

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  future: {},
-  purge: [],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
