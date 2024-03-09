99cf10c90a87090716f25b0017ea558d1298aa94
Jayatri Banerjee <jbanerjeedgp01022003@gmail.com>
2024-03-10 04:08:58 +0530

    add user admin form

---
+++ client/src/dashboard/admin-panel.tsx
@@ -0,0 +1,11 @@
+import { CreateUserFormDemo } from './create-users';
+
+function createUserPage() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center ">
+      <CreateUserFormDemo />
+    </div>
+  );
+}
+
+export default createUserPage;
