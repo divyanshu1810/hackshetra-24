341696f1dcb1468b8aa1fb15504b5e55f1f7e16f
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-03-09 21:33:39 +0530

    fix: landing-page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,11 +12,13 @@ function App() {
 				<Routes>
 					<Route path="/" element={<SignupPage />} />
 					<Route path="/login" element={<LoginFormDemo />} />
+          <Route path="/signup" element={<SignupPage />} />
 					<Route path="/new-transit" element={<NewTransit />} />
 					<Route path="/view-transits" element={<ViewTransits />} />
 				</Routes>
 			</BrowserRouter>
 		</>
 	);
-	}
+}
+
 export default App;
