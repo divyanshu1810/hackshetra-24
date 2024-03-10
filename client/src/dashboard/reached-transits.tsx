6ae1cf381d410cef6d2376eff570dc155d4bf319
tanmayagrwl <tanmayhp78@gmail.com>
2024-03-10 13:01:57 +0530

    update nav in reached transits

--- client/src/dashboard/reached-transits.tsx
+++ client/src/dashboard/reached-transits.tsx
@@ -5,7 +5,52 @@ import { useState } from "react";
 export default function ReachedTransits() {
 	const [status, setStatus] = useState("");
 	return (
-		<div>
+		<>	<nav className="bg-gray-800 p-4">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <span className="text-white font-semibold text-lg">TrackMYTransit</span>
+            </div>
+            {localStorage.getItem('role') === 'admin' || localStorage.getItem('role') === 'receptionist' ? (
+              <Link to="/dashboard">
+  <div className="text-white font-semibold text-lg">
+    Incoming Transits
+  </div></Link>
+) : (
+  <div></div>
+)}
+ {localStorage.getItem('role') === 'admin' || localStorage.getItem('role') === 'factory-worker' ? (
+  <Link to="/dashboard/reached-transits">
+  <div className="text-white font-semibold text-lg">
+    Reached Transits
+  </div>
+  </Link>
+) : (
+  <div></div>
+)}
+            
+            {localStorage.getItem('role') === 'admin' || localStorage.getItem('role') === 'factory-worker' ? (
+  <Link to="/dashboard/admin-panel">
+  <div className="text-white font-semibold text-lg">
+   Admin Pannel
+  </div>
+  </Link>
+) : (
+  <div></div>
+)}
+            <button
+              onClick={() => {
+                localStorage.setItem('isLoggedIn', 'false');
+                localStorage.setItem('token', '');
+                localStorage.setItem('role', '');
+                localStorage.setItem('name', '');
+                window.location.href = '/login';
+              }}
+              className="text-white font-semibold text-lg bg-red-500 px-4 py-1 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600"
+            >Log Out</button>
+          </div>
+        </div>
+      </nav>	<div>
 			
 
 			<div className="w-full flex gap-5 flex-col max-w-xl rounded-xl m-5 p-5 bg-orange-100">
@@ -61,6 +106,6 @@ export default function ReachedTransits() {
 				</form>
 				
 			</div>
-		</div>
+		</div></>
 	);
 }
