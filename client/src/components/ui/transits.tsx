8ec928203f714669e7dfc743853938e34f03e50a
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-03-10 10:55:50 +0530

    fix: lint

--- client/src/components/ui/transits.tsx
+++ client/src/components/ui/transits.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface Transit {
   _id: string;
@@ -28,13 +28,17 @@ const TransitCard: React.FC<TransitCardProps> = ({ transit }) => {
       <p><strong>Contact Number:</strong> {transit.contact_number}</p>
       <p><strong>Origin:</strong> {transit.origin}</p>
       <p><strong>Destination:</strong> {transit.destination}</p>
-      <Link to={`/transit/${transit._id}`}>View Details</Link>
+      <Link to={`/transit/${transit._id}`}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          View Details
+        </button>
+      </Link>
     </div>
   );
 };
 
 
-  
+
 
 interface TransitCardListProps {
   data: Transit[];
